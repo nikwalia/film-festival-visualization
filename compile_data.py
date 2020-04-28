303ec09c0bddcaf2042e753ff622ba4a183efca2
nikwalia <nikash.walia@gmail.com>
2020-04-27 22:56:35 -0700

    Extract data from files

--- compile_data.py
+++ compile_data.py
@@ -1,6 +1,22 @@
 import pandas as pd
-import numpy as np
+import os
+
+
+def find_year_data(year):
+    all_files = os.listdir('data')
+    year_data = {}
+    for file in all_files:
+        df = pd.read_csv('data/' + file, delimiter='\t', index_col=None)
+        festival_name = file.split('_')[0]
+        award_category = file.split('_')[2].split('.')[0]
+        matches = df[df['Year'] == year]
+        if festival_name not in year_data.keys():
+            year_data[festival_name] = {}
+        year_data[festival_name][award_category] = matches
+
+    return year_data
+
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/venice_golden_lion.tsv', delimiter='\t')
-    print(df)
+    dat = find_year_data(2001)
+    print(dat)

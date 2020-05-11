bad2d14e25216a1d27be7b77e0bcd573b60e1692
Pranav Burugula <pranav.burugula@gmail.com>
2020-05-10 22:19:07 -0400

    Fixed gui

--- gui.py
+++ gui.py
@@ -8,24 +8,29 @@ start_year = 1932
 end_year = 2019
 festival = "Cannes"
 year = start_year
-
+world_map = None
 def dropdown_handler(attr, old, new):
+    global festival
     festival = new
+    global world_map
+    world_map = render_map.plot_data(festival.lower(), year)
 
 def year_handler(attr, old, new):
+    global year
     year = new
-
-if __name__ == "__main__":
-    output_file("film-festivals.html")
-    slider = Slider(start=start_year, end=end_year, value=year, step=1, title="Year")
-    slider.on_change("value", year_handler)
-    # show(slider)
-
-    festival_list = [("Cannes", "cannes"), ("Berlin", "berlin"), ("Venice", "venice")]
-    festival_dropdown = Dropdown(label=festival, button_type="primary", menu=festival_list)
-    festival_dropdown.on_change("value", dropdown_handler)
-    # show(festival_dropdown)
+    global world_map
     world_map = render_map.plot_data(festival.lower(), year)
-    layout = column(row(widgetbox(festival_dropdown), widgetbox(slider)), world_map)
-    curdoc().add_root(layout)
-    show(layout)
+
+output_file("film-festivals.html")
+slider = Slider(start=start_year, end=end_year, value=start_year, step=1, title="Year")
+slider.on_change("value", year_handler)
+# show(slider)
+
+festival_list = [("Cannes", "cannes"), ("Berlin", "berlin"), ("Venice", "venice")]
+festival_dropdown = Dropdown(label=festival, button_type="primary", menu=festival_list)
+festival_dropdown.on_change("value", dropdown_handler)
+# show(festival_dropdown)
+world_map = render_map.plot_data(festival.lower(), year)
+layout = column(row(widgetbox(festival_dropdown), widgetbox(slider)), world_map)
+curdoc().add_root(layout)
+show(layout)
